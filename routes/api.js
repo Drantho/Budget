4c236b7fe21b4a1c363a539fee6d31a52c5f7aaf
Drantho <drantho@gmail.com>
2021-02-10 18:10:06 -0800

    bug fix

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,15 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-router.get("/:all", (req, res)=> {
-  res.json({msg: "hello"});
-})
-
 router.post("/api/transaction", ({body}, res) => {
-  Transaction.create(body)
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
+  res.redirect("http://www.google.com");
+  // Transaction.create(body)
+  //   .then(dbTransaction => {
+  //     res.json(dbTransaction);
+  //   })
+  //   .catch(err => {
+  //     res.status(404).json(err);
+  //   });
 });
 
 router.post("/api/transaction/bulk", ({body}, res) => {

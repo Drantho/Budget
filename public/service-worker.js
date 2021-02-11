38c6a5fc01ab0f7a2791e7810b94fed4abba0737
Drantho <drantho@gmail.com>
2021-02-10 17:12:24 -0800

    bug fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,14 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/db.js",
-  "/index.js",
-  "/manifest.webmanifest",
-  "/styles.css",
-  "/js/loadImages.js",
-  "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
+    "/",
+    "/index.html",
+    "/db.js",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
 ];
 
 // install

4d1ff2121462d259749523c07ec2df4cf70616ee
Drantho <drantho@gmail.com>
2021-02-10 18:34:27 -0800

    bug fix

--- routes/api.js
+++ routes/api.js
@@ -23,13 +23,14 @@ router.post("/api/transaction/bulk", ({body}, res) => {
 });
 
 router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
+  res.json({msg: "nothing works!!!!!!!!!"})
+  // Transaction.find({}).sort({date: -1})
+  //   .then(dbTransaction => {
+  //     res.json(dbTransaction);
+  //   })
+  //   .catch(err => {
+  //     res.status(404).json(err);
+  //   });
 });
 
 module.exports = router;

92fccfa81b746594d3785d4f98c8fd18c271d4b4
Drantho <drantho@gmail.com>
2021-02-10 17:15:33 -0800

    bug fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,7 @@ const FILES_TO_CACHE = [
     "/db.js",
     "/index.js",
     "/manifest.webmanifest",
-    "/styles.css",
+    "/style.css",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
 ];
